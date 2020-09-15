23702e0ae70bbf8b44740aed54b1f4d719cca701
mjordan <michael@glowingpixel.com>
2020-09-15 16:50:19 -0700

    Anagrams

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,18 +1,3 @@
 def find_anagrams(word, candidates):
 	"""Return words that are anagrams of the original"""
-	anas = []
-
-	word = word.casefold()
-	
-	for idx, cand in enumerate(candidates):
-		cand = cand.casefold()
-
-		# Skip if word is identical
-		if word == cand:
-			continue
-		
-		# Word is identical if list of sorted letters are the same
-		elif sorted(word) == sorted(cand):
-			anas.append(candidates[idx])
-	
-	return anas
+	return [candidate for candidate in candidates if candidate.casefold() != word.casefold() and sorted(candidate.casefold()) == sorted(word.casefold())]
